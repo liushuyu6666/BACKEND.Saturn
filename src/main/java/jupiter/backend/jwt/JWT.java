6f742e867bcfa97875c33497f5660accaf40af5e
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-01-27 18:36:19 -0500

    modify login

---
+++ src/main/java/jupiter/backend/jwt/JWT.java
@@ -0,0 +1,56 @@
+package jupiter.backend.jwt;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jupiter.backend.user.UserDetail;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class JWT {
+
+    @Value("${jupiter.jwt.token")
+    private String plainSecret;
+
+    @Value("${jupiter.jwt.expire-in-hours}")
+    private Long expiration;
+
+    private String superSecret(){
+        return Base64.getEncoder().encodeToString(this.plainSecret.getBytes());
+    }
+
+    public String createToken(String username, String role){
+
+        Date now = new Date();
+        Date expireAt = new Date(now.getTime() + TimeUnit.HOURS.toMillis(this.expiration));
+
+
+        return Jwts.builder()
+                .setId(UUID.randomUUID().toString())
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expireAt)
+                .signWith(SignatureAlgorithm.HS256, superSecret())
+                .claim("details", new UserDetail(username, role))
+                .compact();
+    }
+
+    public LinkedHashMap<String, String> verifyToken(String token) throws Exception{
+        try{
+            Claims claims = Jwts.parser()
+                    .setSigningKey(superSecret())
+                    .parseClaimsJws(token)
+                    .getBody();
+            return (LinkedHashMap) claims.get("details");
+        }
+        catch(Exception e){
+            throw new Exception(e);
+        }
+    }
+
+}
---
+++ src/main/java/jupiter/backend/loginrequest/LoginRequest.java
@@ -0,0 +1,38 @@
+package jupiter.backend.loginrequest;
+
+import jupiter.backend.jwt.JWT;
+import jupiter.backend.user.User;
+import jupiter.backend.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginRequest {
+
+    @Autowired
+    JWT jwt;
+
+    @Autowired
+    UserRepository userRepository;
+
+    public String createToken(LoginRequestBody loginRequestBody) throws Exception{
+        String loginUsername = loginRequestBody.getUsername();
+        String loginPassword = loginRequestBody.getPassword();
+        String loginRole     = loginRequestBody.getRole();
+        User foundUser = userRepository.findUserByUsernameAndRole(loginUsername, loginRole);
+
+        if(foundUser != null){
+            if(foundUser.getPassword().equals(loginPassword)){
+                return jwt.createToken(loginUsername, foundUser.getRole());
+            }
+            else{
+                throw new Exception("password and username don't match");
+            }
+        }
+        else{
+            if(loginUsername == null) throw new Exception("username can't be empty");
+            else throw new Exception("no such user");
+        }
+    }
+
+}
