9e12956a8901586aa2da1c935d19916213a2bfbe
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-01-27 18:31:06 -0500

    enable mongo auditing

--- src/main/java/jupiter/backend/LiushuyuApplication.java
+++ src/main/java/jupiter/backend/LiushuyuApplication.java
@@ -2,13 +2,15 @@ package jupiter.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
 
 @SpringBootApplication()
+@EnableMongoAuditing
 public class LiushuyuApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LiushuyuApplication.class, args);
 	}
 
