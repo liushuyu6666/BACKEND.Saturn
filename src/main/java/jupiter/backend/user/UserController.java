3bbd6398016b57b4b973cda0fa4e705bcfe225fc
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-01-27 18:25:56 -0500

    User and Usercontroller

---
+++ src/main/java/jupiter/backend/user/UserController.java
@@ -0,0 +1,81 @@
+package jupiter.backend.controller;
+
+
+import io.jsonwebtoken.Claims;
+import jupiter.backend.core.JWT;
+import jupiter.backend.model.loginrequest.LoginRequest;
+import jupiter.backend.model.loginrequest.LoginRequestBody;
+import jupiter.backend.model.user.User;
+import jupiter.backend.model.user.UserDetail;
+import jupiter.backend.model.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import jupiter.backend.response.ResponseBody;
+
+import java.util.LinkedHashMap;
+
+@RequestMapping("/v1")
+@RestController
+@CrossOrigin(origins = "*")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    JWT jwt;
+
+    @Autowired
+    LoginRequest loginRequest;
+
+    @PostMapping("/register")
+    public ResponseEntity<ResponseBody> createUser(@RequestBody User userBody){
+        try{
+            User newUser = userService.createUser(userBody);
+            ResponseBody<User> responseBody = new ResponseBody<>(newUser,
+                    "create user successfully", null);
+            return ResponseEntity.ok(responseBody);
+
+        } catch(Exception e){
+            ResponseBody<User> responseBody = new ResponseBody<>(null,
+                    e.getMessage(), e);
+            return ResponseEntity.ok(responseBody);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ResponseBody> login(@RequestBody LoginRequestBody loginRequestBody){
+        try{
+            String token = loginRequest.createToken(loginRequestBody);
+            ResponseBody responseBody = new ResponseBody(token,
+                    "token has been wrapped", null);
+            return ResponseEntity.ok(responseBody);
+        }
+        catch(Exception e){
+            ResponseBody responseBody = new ResponseBody(null,
+                    e.getMessage(), e);
+            return ResponseEntity.ok(responseBody);
+        }
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<ResponseBody> checkProfile(@RequestHeader("token") String token){
+        try{
+            LinkedHashMap<String, String> userDetail = jwt.verifyToken(token);
+            // Should be user info
+            User currUser = userService.retrieveUserSafely(userDetail.get("username"),
+                    userDetail.get("role"));
+            ResponseBody responseBody = new ResponseBody(currUser, null, null);
+            return ResponseEntity.ok(responseBody);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+            ResponseBody responseBody = new ResponseBody(null, e.getMessage(), e);
+            return ResponseEntity.ok(responseBody);
+        }
+    }
+
+
+}
