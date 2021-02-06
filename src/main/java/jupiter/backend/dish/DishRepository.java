3d85b6c1d812951eca5a521201db04800da487fe
liushuyu6666 <liushuyu6666@users.noreply.github.com>
2021-02-06 01:30:44 -0500

    dish

--- src/main/java/jupiter/backend/dish/DishRepository.java
+++ src/main/java/jupiter/backend/dish/DishRepository.java
@@ -1,5 +1,6 @@
 package jupiter.backend.dish;
 
+import com.mongodb.BasicDBObject;
 import jupiter.backend.shop.Shop;
 import jupiter.backend.shop.ShopRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +12,19 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 interface CustomizedDishRepository{
     Dish updateDish(String shopId, Dish dish);
+
+    void createDish(String shopId, Dish dish);
+
+    void deleteDish(String shopId, String dishId);
+
+    Dish findDishByName(String shopId, String dishName);
+
+    Dish findDishById(String shopId, String dishId);
 }
 
 class CustomizedDishRepositoryImpl implements CustomizedDishRepository{
@@ -23,10 +35,21 @@ class CustomizedDishRepositoryImpl implements CustomizedDishRepository{
     @Autowired
     ShopRepository shopRepository;
 
+    public void createDish(String shopId, Dish dish){
+        Query query = new Query(Criteria.where("_id").is(shopId));
+        Update update = new Update().addToSet("dishes", dish);
+        mongoTemplate.updateFirst(query, update, Shop.class);
+    }
+
     public Dish updateDish(String shopId, Dish dish){
         Query query = new Query(new Criteria().andOperator(
                 Criteria.where("_id").is(shopId),
-                Criteria.where("dishes").elemMatch(Criteria.where("_id").is(dish.get_id()))));
+                Criteria.where("dishes")
+                        // because use elemMatch, only the matched element appears
+                        // that's why update addToSet could find this element.
+                        .elemMatch(Criteria
+                                .where("_id")
+                                .is(dish.get_id()))));
 //        With the use of the $ array projection operator,
 //        you can specify the projection to return the first
 //        element that match the query condition on the array field
@@ -48,6 +71,56 @@ class CustomizedDishRepositoryImpl implements CustomizedDishRepository{
                 .findFirst()
                 .orElse(null);
     }
+
+    public void deleteDish(String shopId, String dishId){
+        Query query = new Query(new Criteria().andOperator(
+                Criteria.where("_id").is(shopId),
+                Criteria.where("dishes")
+                        .elemMatch(Criteria
+                                .where("_id")
+                                .is(dishId))));
+        Update update = new Update().pull("dishes",
+                new BasicDBObject("_id", dishId));
+        mongoTemplate.updateMulti(query, update, Shop.class);
+    }
+
+    public Dish findDishByName(String shopId, String dishName){
+        Query query = new Query(new Criteria().andOperator(
+                Criteria.where("_id").is(shopId),
+                Criteria.where("dishes").elemMatch(Criteria.where("name").is(dishName))
+        ));
+        List<Shop> shopList = mongoTemplate.find(query, Shop.class);
+        Shop shop = shopList.stream().findFirst().orElse(null);
+        if(shop == null){
+            return null;
+        }
+        List<Dish> dishList =
+                shop.getDishes()
+                .stream()
+                .filter(dish -> dish.getName()
+                        .equals(dishName))
+                .collect(Collectors.toList());
+        return dishList.get(0);
+    }
+
+    public Dish findDishById(String shopId, String dishId){
+        Query query = new Query(new Criteria().andOperator(
+                Criteria.where("_id").is(shopId),
+                Criteria.where("dishes").elemMatch(Criteria.where("_id").is(dishId))
+        ));
+        List<Shop> shopList = mongoTemplate.find(query, Shop.class);
+        Shop shop = shopList.stream().findFirst().orElse(null);
+        if(shop == null){
+            return null;
+        }
+        List<Dish> dishList =
+                shop.getDishes()
+                        .stream()
+                        .filter(dish -> dish.get_id()
+                                .equals(dishId))
+                        .collect(Collectors.toList());
+        return dishList.get(0);
+    }
 }
 
 @Repository
@@ -55,15 +128,11 @@ public interface DishRepository extends MongoRepository<Dish, String>,
         CustomizedDishRepository
 {
 
-    // list all dishes under the shop
-//    @Query(value = "{'_id': ?0}", fields = "{'dishes': 1}")
-//    Dish listAllDishesInShop(String shopId);
-
     @org.springframework.data.mongodb.repository.Query(value = "{'_id': ?0}")
     Shop listAllDishesInShop(String id);
 
-    // find a specific dish in the shop
-    @org.springframework.data.mongodb.repository.Query(value = "{'_id': ?0, 'dishes.name': ?1}", fields = "{'dishes': 1}")
-    Dish findDishByName(String shopId, String DishName);
+//    // find a specific dish in the shop
+//    @org.springframework.data.mongodb.repository.Query(value = "{'_id': ?0, 'dishes.name': ?1}", fields = "{'dishes': 1}")
+//    Dish findDishByName(String shopId, String DishName);
 
 }
--- src/main/java/jupiter/backend/dish/DishService.java
+++ src/main/java/jupiter/backend/dish/DishService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service
@@ -20,11 +19,11 @@ public class DishService {
     @Autowired
     ShopRepository shopRepository;
 
-    public Dish createDish(String shopId, Dish newDish) throws LsyException{
-        Shop shop = shopRepository.findBy_id(shopId);
+    public Dish createDish(String shopId, String ownerId, Dish newDish) throws LsyException{
+        Shop shop = shopRepository.findBy_idAndOwnerId(shopId, ownerId);
         Shop testDish = shopRepository.findShopBy_idAndDishName(shopId, newDish.getName());
         if(shop == null){
-            throw new LsyException("no such shop");
+            throw new LsyException("no such shop under this owner");
         }
         if(testDish != null){
             throw new LsyException("dish name existed");
@@ -37,10 +36,9 @@ public class DishService {
             dish.setDesc(newDish.getDesc());
             dish.setCategory(newDish.getCategory());
             dish.setPrice(newDish.getPrice());
-            dish.setReview(newDish.getReview());
+            dish.setReview(new ArrayList<>());
             dish.setModifiedAt();
-            shop.getDishes().add(dish);
-            shopRepository.save(shop);
+            dishRepository.createDish(shopId, dish);
             return dish;
         }
         catch(Exception e){
@@ -48,10 +46,59 @@ public class DishService {
         }
     }
 
+//    public Dish createDish(String shopId, Dish newDish) throws LsyException{
+//        Shop shop = shopRepository.findBy_id(shopId);
+//        Shop testDish = shopRepository.findShopBy_idAndDishName(shopId, newDish.getName());
+//        if(shop == null){
+//            throw new LsyException("no such shop");
+//        }
+//        if(testDish != null){
+//            throw new LsyException("dish name existed");
+//        }
+//        try{
+//            Dish dish = new Dish();
+//            dish.set_id();
+//            dish.setName(newDish.getName());
+//            dish.setImgUrl(newDish.getImgUrl());
+//            dish.setDesc(newDish.getDesc());
+//            dish.setCategory(newDish.getCategory());
+//            dish.setPrice(newDish.getPrice());
+//            dish.setReview(newDish.getReview());
+//            dish.setModifiedAt();
+//            shop.getDishes().add(dish);
+//            shopRepository.save(shop);
+//            return dish;
+//        }
+//        catch(Exception e){
+//            throw e;
+//        }
+//    }
+
     public Dish retrieveDish(String shopId, String dishId) throws LsyException{
         Shop findShop = shopRepository.findShopBy_idAndDish_id(shopId, dishId);
         if(findShop == null){
             throw new LsyException("no such shop or dish");
+        }
+        try{
+            Dish findDish = findShop
+                    .getDishes()
+                    .stream()
+                    .filter(dish -> dish
+                            .get_id()
+                            .equals(dishId))
+                    .findFirst()
+                    .orElse(null);
+            return findDish;
+        }
+        catch (Exception e){
+            throw e;
+        }
+    }
+
+    public Dish retrieveDish(String shopId, String dishId, String ownerId) throws LsyException{
+        Shop findShop = shopRepository.findShopBy_idDish_idAndOwnerId(shopId, dishId, ownerId);
+        if(findShop == null){
+            throw new LsyException("no such shop or dish under the login owner");
         }
         try{
             Dish findDish = findShop
@@ -83,21 +130,56 @@ public class DishService {
         }
     }
 
-    public Dish updateDish(String shopId, Dish dish) throws LsyException{
-        Shop shop = shopRepository.findShopBy_idAndDishName(shopId, dish.getName());
+    public List<Dish> listDishes(String shopId, String ownerId) throws LsyException{
+        Shop shop = shopRepository.findBy_idAndOwnerId(shopId, ownerId);
+        if(shop == null){
+            throw new LsyException("no such shop under this login owner");
+        }
+        try{
+            List<Dish> dishes = shop.getDishes();
+            return dishes;
+        }
+        catch (Exception e){
+            throw e;
+        }
+    }
+
+    public Dish updateDish(String shopId, String ownerId, Dish dish) throws LsyException{
+        Shop shop = shopRepository.findShopBy_idDish_idAndOwnerId(shopId, dish.get_id(), ownerId);
+        Dish duplicateNameDish = dishRepository.findDishByName(shopId, dish.getName());
         if(shop != null){
-            throw new LsyException("new dish name existed");
+            try{
+                if(duplicateNameDish == null ||
+                        duplicateNameDish.get_id().equals(dish.get_id())){
+                    return dishRepository.updateDish(shopId, dish);
+                }
+                else{
+                    throw new LsyException("new dish name existed in this shop");
+                }
+            }
+            catch(Exception e){
+                throw e;
+            }
+        }
+        else{
+            throw new LsyException("no such shop or dish under this login owner");
+        }
+    }
+
+    public boolean deleteDish(String shopId, String dishId, String ownerId) throws LsyException{
+        Shop shop = shopRepository.findShopBy_idDish_idAndOwnerId(shopId, dishId, ownerId);
+        if(shop == null){
+            throw new LsyException("no such shop or dish under this login owner");
         }
         else{
             try{
-                Dish updatedDist = dishRepository.updateDish(shopId, dish);
-                return updatedDist;
+                dishRepository.deleteDish(shopId, dishId);
+                return true;
             }
-            catch (Exception e){
+            catch(Exception e){
                 throw e;
             }
         }
     }
 
-    
 }
